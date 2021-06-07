67387ef180ce872ab6f96eb3baa1e33e5fac3358
Abdulmedžid Pehlić <abdulmedzidpehlic@gmail.com>
2021-06-07 18:41:33 +0200

    Initial commit

---
+++ app/src/main/java/com/whoopedu/vrime/util/WeatherJsonUtils.java
@@ -0,0 +1,21 @@
+package com.whoopedu.vrime.util;
+
+public class WeatherJsonUtils {
+
+    public static final String TAG = WeatherJsonUtils.class.getSimpleName();
+
+    /**
+     * This method parses JSON from a web response and returns an array of Strings
+     * describing the weather over various days from the forecast.
+     * <p/>
+     * Later on, we'll be parsing the JSON into structured data within the
+     * getFullWeatherDataFromJson function, leveraging the data we have stored in the JSON. For
+     * now, we just convert the JSON into human-readable strings.
+     *
+     * @param forecastJsonStr JSON response from server
+     *
+     * @return Array of Strings describing weather data
+     *
+     * @throws JSONException If JSON data cannot be properly parsed
+     */
+}
